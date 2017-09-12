8577f9104df1b82576d454f5accfd47d82e23c5a
kobis <kobis@DESKTOP-CRHS60C>
2017-09-12 13:09:49 +0300

    complete video part 2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,37 +1,116 @@
+
+// Get from local storage or initialize
+var data = (localStorage.getItem('todo-list'))? JSON.parse(localStorage.getItem('todo-list')) : {
+    todo: [],
+    completed: []
+}
+
+ // Remove and complete icons in SVG format
+ var removeSVG='<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><g><g><path class="st0" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+ var completeSVG='<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+
+ renderTodoList();
+
+function renderTodoList() {
+    if (!data.todo.length&&!data.completed.length) return;
+
+    for (var i = 0; i < data.todo.length; i++) {
+        var element = data.todo[i];
+        addItemTodo(element);
+    }
+
+    for (var j = 0; j < data.completed.length; j++) {
+        var element = data.completed[j];
+        addItemTodo(element,true);
+    }
+}
+
 // User clicked on the add button
 // If there is any text in the item field, add that text to the todo list
 document.getElementById('add').addEventListener('click', function(){
     var value=document.getElementById('item').value;
     if (value) {
+        
        addItemTodo(value);
-    //   reset the value
-    document.getElementById('item').value='';
-    }
+        //   reset the value
+        document.getElementById('item').value='';
+
+        // Update data object      
+        data.todo.push(value);
+    
+        // Update localStorage
+        DataObjectUpdated();
+  
+}
     
 });
+
+// Add enter event
+document.getElementById('item').addEventListener('keydown',function(e) {
+    var value=this.value;
+    if(e.key==='Enter'&&value) {
+        document.getElementById('add').click();
+    }
+});
+
+function DataObjectUpdated() {
+    localStorage.setItem('todo-list',JSON.stringify(data));
+}
 
 function removeItem() {
     var item=this.parentNode.parentNode;
     var parent=item.parentNode;
+    var id=parent.id;
+    var value=item.innerText;
+
+    // Remove from data object
+    if (id==='todo') {
+        data.todo.splice(data.todo.indexOf(value),1);
+    } else {
+        data.completed.splice(data.todo.indexOf(value),1);
+    }
+
+    console.log(data);
+    // Remove from DOM
     parent.removeChild(item);
+
+    // Update localStorage
+    DataObjectUpdated();
 }
 
 function completeItem() {
     var item=this.parentNode.parentNode;
     var parent=item.parentNode;
     var id = parent.id;
+    var value=item.innerText;
+    var target;
 
-    var target = (id==='todo')? document.getElementById('completed'):document.getElementById('todo');
+    // Update data object
+    if (id==='todo') {
+        target=document.getElementById('completed');
+        data.todo.splice(data.todo.indexOf(value),1);
+        data.completed.push(value);
+    } else {
+        target=document.getElementById('todo');
+        data.completed.splice(data.todo.indexOf(value),1);
+        data.todo.push(value);
+    }
 
+    // Update DOM
     parent.removeChild(item);
     target.insertBefore(item, target.childNodes[0]);
+
+    // Update localStorage
+    DataObjectUpdated();
     
 }
 
-function addItemTodo(text) {
-    var list = document.getElementById('todo');
+function addItemTodo(text,completed) {
+    var list = completed? document.getElementById('completed'): document.getElementById('todo');
     var item = document.createElement('li');
     item.innerText=text;
+
+    
     
     // Remove and complete buttons for each item
     var buttons = document.createElement('div');
@@ -42,10 +121,6 @@ function addItemTodo(text) {
 
     var complete = document.createElement('button');
     complete.classList.add('complete');
-
-    // Remove and complete icons in SVG format
-    var removeSVG='<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><g><g><path class="st0" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-    var completeSVG='<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
     remove.innerHTML=removeSVG;
     complete.innerHTML=completeSVG;
@@ -61,6 +136,7 @@ function addItemTodo(text) {
 
     list.insertBefore(item,list.childNodes[0]);
 
-
+    
 
 }
+
